e288909e2fa819a8e6537bd3e872c63db6ad25a2
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-09-20 13:12:40 -0300

    BaSP-W04-A2022: ex02-strings(changes)

--- Semana-04/exercises/02_strings.js
+++ Semana-04/exercises/02_strings.js
@@ -21,8 +21,7 @@ y generar un nuevo string con los últimos 3 caracteres guardando
 el resultado en una nueva variable (utilizar substring).*/
 
 var text3="PersonalComputer";
-var lenghtOfText=text3.length;
-var resultExerciseC=text3.substring(lenghtOfText-3,lenghtOfText);
+var resultExerciseC=text3.substring(text3.length-3,text3.length);
 console.log("ex-02c: The last 3 letters of the string are:",resultExerciseC);
 
 /*d) Crear una variable de tipo string con al menos 10 caracteres
@@ -31,9 +30,7 @@ demás en minúscula. Guardar el resultado en una nueva variable
 (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
 
 var text4="fullstackDEveloper";
-var firstLetter =text4.substring(0,1);
-var restOfTheText=text4.substring(1,text4.length)
-var resultExerciseD=firstLetter.toUpperCase()+restOfTheText.toLowerCase();
+var resultExerciseD=text4.substring(0,1).toUpperCase()+text4.substring(1,text4.length).toLowerCase();
 console.log("ex-02d: The string with the first letter capitalized is:",resultExerciseD);
 
 /*e) Crear una variable de tipo string con al menos 10 caracteres
@@ -53,10 +50,6 @@ y el operador +).*/
 
 var text6="learnING javaSCript";
 var spaceIndex=text6.indexOf(" ");
-var letterFirstWord=text6.substring(0,1);
-var restOfTheFirstWord=text6.substring(1,spaceIndex);
-var letterSecondWord=text6.substring(spaceIndex+1,spaceIndex+2);
-var restOfTheSecondWord=text6.substring(spaceIndex+2);
-var resultExerciseF=letterFirstWord.toUpperCase()+restOfTheFirstWord.toLowerCase()+" "+letterSecondWord.toUpperCase()+
-    restOfTheSecondWord.toLowerCase();
+var resultExerciseF=text6.substring(0,1).toUpperCase()+text6.substring(1,spaceIndex).toLowerCase()+" "
+                +text6.substring(spaceIndex+1,spaceIndex+2).toUpperCase()+text6.substring(spaceIndex+2).toLowerCase();
 console.log("ex-02f: The result of this excercise is:",resultExerciseF);
