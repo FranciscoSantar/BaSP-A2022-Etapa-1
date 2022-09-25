3882d0b9dab547d085751ef469943be075d6de88
Francisco Santarelli <franciscosantarelli@gmail.com>
2022-09-25 15:08:40 -0300

    BaSP-W06: Made the landing for the sign un page

--- Semana-06/javascript/login.js
+++ Semana-06/javascript/login.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-    var form = document.getElementById("logInForm");
+    var form = document.getElementById("login-form");
     var label = document.getElementsByTagName("label");
-    var email = document.getElementById("logInMail");
-    var password = document.getElementById("logInPassword");
-    var sumbit = document.getElementById("sumbitButton");
+    var email = document.getElementById("login-email");
+    var password = document.getElementById("login-password");
+    var sumbit = document.getElementById("sumbit-button");
     var errorParagraphEmail = document.createElement('p');
     var errorParagraphPassword = document.createElement('p');
 
@@ -25,19 +25,24 @@ window.onload = function () {
         e.preventDefault();
         if (!checkEmail()){
             alert("The Email has an error. Please fix it");
+            return false;
         }
         if(!checkPassword()){
             alert("The Password has an error. Please fix it");
+            return false;
         }
         else{
-            alert("Email: " + email.value + "\n" + "Password: " + password.value);
+            alert("Email: " + email.value + "\nPassword: " + password.value);
             return true;
         }
-        return false;
     }
 //------------------------------------------------------------------------
     function checkEmail (){
         var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+        if (email.value.length==0){
+            email.classList.add("red-border");
+            return false;
+        }
         if (!emailExpression.test(email.value)){
             email.classList.add("red-border");
             errorParagraphEmail.textContent = "Wrong format of the Email, please fix it.";
@@ -52,18 +57,26 @@ window.onload = function () {
     }
 
     function checkPassword(){
-        for (var i=0;i<password.value.length;i++){
-            if (password.value.charCodeAt(i) < 48 || password.value.charCodeAt(i) > 122 ||
-               (password.value.charCodeAt(i) > 57 && password.value.charCodeAt(i) < 65) ||
-               (password.value.charCodeAt(i) > 90 && password.value.charCodeAt(i) < 97)){
+        if (!password.value.length){
+            password.classList.add("red-border");
+            return false;
+        }
+        else {
+            for (var i=0;i<password.value.length;i++){
+                if (password.value.charCodeAt(i) < 48 || password.value.charCodeAt(i) > 122 ||
+                (password.value.charCodeAt(i) > 57 && password.value.charCodeAt(i) < 65) ||
+                (password.value.charCodeAt(i) > 90 && password.value.charCodeAt(i) < 97)){
                     password.classList.add("red-border");
                     errorParagraphPassword.textContent = "Wrong format of the Password, please fix it.";
                     errorParagraphPassword.classList.add("error-text");
                     form.insertBefore(errorParagraphPassword, sumbit);
                     return false;
                 }
+            }
         }
         password.classList.add("green-border");
+        console.log(password.value.length)
+        console.log(email.value.length)
         return true;
     }
 }
